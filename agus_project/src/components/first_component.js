12860787a6d01a29c57cf185d572c66c557a17a1
Pedro Agustin Darboure <119804246+pdarboure@users.noreply.github.com>
2023-05-01 16:35:05 -0300

    creacion primer componente

---
+++ agus_project/src/components/first_component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function first_Component() {
+    return (
+    <h1>
+        Soy el primer component
+     </h1>
+    );
+  }
+  
+  export default first_Component;
+
--- agus_project/src/index.js
+++ agus_project/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import './App.css';
 import reportWebVitals from './reportWebVitals';
+import Component from './components/first_component';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<div>
+    <Component/>
+</div>
 );
 
 // If you want to start measuring performance in your app, pass a function
